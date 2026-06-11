215379a1f0ba2ba7f29c3be5ffb2855e5fc3e139
Oisín Tohak <oisintohak@gmail.com>
2026-06-11 20:59:00 +0100

    chore: add slug field

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,11 @@ class Post(models.Model):
     """
 
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+    )
+    subtitle = models.CharField(max_length=200, null=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
