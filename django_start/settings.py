433292df3feb6eb5fe17b7f1bfffeedef92a2a7b
Oisín Tohak <oisintohak@gmail.com>
2026-07-14 21:18:19 +0100

    chore: update settings.py

--- django_start/settings.py
+++ django_start/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("RENDER") not in ["True", "true", "1"]
+DEBUG = os.environ.get("DEBUG") not in ["True", "true", "1"]
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
